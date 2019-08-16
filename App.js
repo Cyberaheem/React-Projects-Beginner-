a156b90197a0f9de8222f68abb14473b0bc2c646
Cyberaheem <54164153+Cyberaheem@users.noreply.github.com>
2019-08-16 12:15:50 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+   
+    constructor() {
+        super();
+        this.state = {
+            rockPaperScissorsComputer: ['Rock', 'Paper', 'Scissors'],
+            rockPaperScissorsUser: null,
+            random: null,
+            outcome: '' // new state for containing the outcome message
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
+    
+    handleClick(type) {
+        this.setState({ outcome: '' }, () => {
+            const min = 0;
+            const max = 3;
+            const random = Math.floor(min + (Math.random() * (max - min)));
+            this.setState({ random })
+            this.setState({
+                rockPaperScissorsUser: type
+            })
+            const tie = (this.state.rockPaperScissorsComputer[random] === type);
+            const win = (
+                ((type === 'Rock') && (random === 2)) ||
+                ((type === 'Paper') && (random === 0)) ||
+                ((type === 'Scissors') && (random === 1))
+            );
+            setTimeout(() => {
+                
+                this.setState({
+                    outcome: (
+                        <h1 className={win && 'ribbon'}>{
+                            tie ? 'It was a tie' :
+                                win ? 'You win' : 'The computer wins'
+                        }</h1>
+                    )
+                })
+            }, 500)
+        })
+    }
+
+
+    render() {
+        return (
+            <div>
+                <button value="Click me!" onClick={() => this.handleClick('Rock')}>Rock</button>
+                <button value="Click me!" onClick={() => this.handleClick('Paper')}>Paper</button>
+                <button value="Click me!" onClick={() => this.handleClick('Scissors')}>Scissors</button>
+
+
+                <h2 className="x">You selected: {this.state.rockPaperScissorsUser}</h2>
+                <h1 className='x'>The computer selected: {this.state.rockPaperScissorsComputer[Math.floor(this.state.random)]}</h1>
+
+                {this.state.outcome}
+            </div>
+        );
+    }
 }
 
-export default App;
+export default App
+
+
